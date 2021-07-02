6cb6513707906f1a3598ea8f33e2bf8e52db5c80
cpan1995 <cpan1995@gmail.com>
2021-07-02 19:37:30 -0400

    Completed Assignment

--- Phase-1/04-events/src/index.js
+++ Phase-1/04-events/src/index.js
@@ -17,7 +17,6 @@ document.querySelector('#book-form').addEventListener('submit',(e)=> {
 function renderAside(storeObj){
   document.querySelector('.location').textContent = storeObj.name
   document.querySelector('#hours').textContent = storeObj.hours
-  console.log(document.querySelectorAll('aside ul li'))
   document.querySelectorAll('aside ul li')[0].textContent = storeObj.number
   document.querySelectorAll('aside ul li')[1].textContent = storeObj.address
 
@@ -31,6 +30,7 @@ function renderCard(book){
   let pPrice = document.createElement('p')
   let pInventory = document.createElement('p')
   let btn = document.createElement('button')
+  
 
   li.className = 'card'
   img.src = book.image_url
@@ -41,7 +41,6 @@ function renderCard(book){
   btn.textContent = 'Add Inventory'
 
   btn.addEventListener('click', () => {
-    console.log('hi')
     book.inventory+=1
     pInventory.textContent = `Inventory: ${book.inventory}`
   })
@@ -49,9 +48,41 @@ function renderCard(book){
   //The books has an array of reviews
   //Add the review content to the card. 
   //Bonus
-  //Add a form to the card that will render a new review to the card. 
+  //Add a form to the card that will render a new review to the card.
 
   li.append(img, h4Title, h4Author, pPrice, pInventory, btn)
+  let counter = 1;
+  book.reviews.forEach(element => {
+    let eachReview = document.createElement('p');
+    eachReview.id = 'reviewChild';
+    eachReview.textContent = `Review ${counter}: ${element.content}`
+    li.append(eachReview);
+    counter +=1;
+  })
+  
+  let formButton = document.createElement('button')
+  formButton.textContent = "Add Review"
+  formButton.setAttribute('type', 'submit');
+  let reviewInput = document.createElement('input');
+  reviewInput.setAttribute('type', 'text');
+  reviewInput.name = 'reviewText';
+  reviewInput.setAttribute('id', "reviewTextBox")
+  reviewInput.setAttribute('style', "border:1px solid #ff0000")
+  let reviewForm = document.createElement('form');
+  reviewForm.id = 'reviewForm';
+
+  reviewForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if(e.target.reviewTextBox.value != null){
+      book.reviews.push({content: e.target.reviewTextBox.value});
+    }
+    renderJustTheReviewsInCards(book,e.currentTarget.parentNode);
+  })
+
+  reviewForm.append(reviewInput, formButton);
+  
+  li.appendChild(reviewForm)
+
   document.querySelector('#book-list').prepend(li)
 
 }
@@ -76,7 +107,6 @@ function renderCard(book){
 
 //Initial Render 
 function initialRender(){
-  console.log(bookStore)
   renderAside(bookStore)
   document.querySelector('#book-list li').remove()
   bookStore.inventory.forEach(renderCard)
@@ -85,5 +115,27 @@ function initialRender(){
   // the above is using renderCard as the callback
   bookStore.inventory.forEach(book => renderCard(book))
 
+}
+
+function renderJustTheReviewsInCards(book, parentElement){
+  let counter = 1
+  removeAllReviewNodes(parentElement);
+  book.reviews.forEach(element => {
+    let eachReview = document.createElement('p');
+    eachReview.id = 'reviewChild';
+    eachReview.textContent = `Review ${counter}: ${element.content}`
+    parentElement.append(eachReview);
+    counter +=1;
+  })
+}
+
+function removeAllReviewNodes(parentElement){
+  console.log(parentElement.querySelectorAll('#reviewChild'))
+  if(parentElement.querySelectorAll('#reviewChild')!=null){
+    parentElement.querySelectorAll('#reviewChild').forEach(element => {
+      element.remove();
+    })
+  }
+  
 }
 initialRender()
